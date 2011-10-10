6fd26f1a4993074f4df593e6a940ecf1a6c49dc4
Adam Venturella <aventurella@gmail.com>
2011-10-10 09:38:38 -0700

    added Rakefile (as best as I understand them)

--- sample/app/partials/partial2.php
+++ sample/app/partials/partial2.php
@@ -6,13 +6,8 @@
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Adam Venturella">
 	<!-- intentionally placing at top to test top and bottom placement -->
-	<script src="/resources/js/modules/project.advertisement.js" module="scripty-modules.min.js"></script>
-	<script src="/resources/js/modules/project.application.js" module="scripty-modules.min.js"></script>
-	<script src="/resources/js/modules/project.services.js" module="scripty-modules.min.js"></script>
-	<script src="/resources/js/modules/project.client1.js" module="scripty-clients-foo.min.js"></script>
-	<script src="/resources/js/modules/project.client2.js" module="scripty-clients-foo.min.js"></script>
 	<!-- testing duplicates -->
-	<script src="/resources/js/lib/jquery.min.js"></script>
+	<script src="/resources/js/jquery/jquery.min.js"></script>
 	<script type ="text/javascript">
 	$(function(){
 		console.log("hi")
@@ -33,9 +28,9 @@
 	<div>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	</div>
-	<script src="/resources/js/lib/jquery.min.js"></script>
+	<script src="/resources/js/jquery/jquery.min.js"></script>
 	<!-- Date: 2011-04-02 -->
-	<script src="/resources/js/application.main.js" minify ></script>
+	<script src="/resources/js/application/application.main.js"></script>
 	<script src="http://domain.com/remote/somescript.js"></script>
 </body>
 </html>
